17b3838fbaca9eac5568111d7e2440181b2a62e9
shabana kauser <shabanakauser89@gmail.com>
2022-09-30 16:52:25 +0100

    Used UseState and added code to store the current item

--- my-todo-list/src/App.js
+++ my-todo-list/src/App.js
@@ -1,13 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [currentItem, setCurrentItem] = useState(null);
+
+  const onChangeHandler = (e) => {
+    console.log("current value", e.target.value);
+    setCurrentItem(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="Wrapper">
           <div className="Input-wrapper">
-            <input />
+            <input value={currentItem} onChange={onChangeHandler} />
             <button>+</button>
           </div>
         </div>
