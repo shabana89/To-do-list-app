d4ae5ba75e0983f213ad13c00aa7175a4f4b716d
shabana kauser <shabanakauser89@gmail.com>
2022-10-05 18:37:40 +0100

    added another use state to store the items entered into the todo list in  an array

--- my-todo-list/src/App.js
+++ my-todo-list/src/App.js
@@ -3,19 +3,22 @@ import { useState } from "react";
 
 function App() {
   const [currentItem, setCurrentItem] = useState(null);
+  const [itemList, updateItemList] = useState([]);
 
   const onChangeHandler = (e) => {
     console.log("current value", e.target.value);
     setCurrentItem(e.target.value);
   };
-
+  const addItemToList = () => {
+    updateItemList([...itemList, currentItem]);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <div className="Wrapper">
           <div className="Input-wrapper">
             <input value={currentItem} onChange={onChangeHandler} />
-            <button>+</button>
+            <button onClick={addItemToList}>+</button>
           </div>
         </div>
       </header>
