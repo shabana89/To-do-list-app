bb12ca6ded0c8cbfc88316f3bb65431665dae5f3
shabana kauser <shabanakauser89@gmail.com>
2022-10-06 19:59:20 +0100

    added div with button inside it marked with an X inside List.js component. This will be for deletoing each item. Will move onto the styling of this next

--- my-todo-list/src/List.js
+++ my-todo-list/src/List.js
@@ -1,9 +1,16 @@
+import "./List.css";
+
 function List(props) {
   console.log("props", props);
   return (
     <div>
       {props.itemList.map((itemObj) => {
-        return <p>{itemObj.item}</p>;
+        return (
+          <div className="item">
+            <p>{itemObj.item}</p>
+            <button>x</button>
+          </div>
+        );
       })}
     </div>
   );
